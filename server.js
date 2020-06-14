653267ca3204bd2ef06d5a45d720bbb6e1fc9920
Your Name <you@example.com>
2020-06-13 20:07:03 -0500

    Prepared for deployment

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ const express = require("express");
 const connectDB = require("./config/db");
 const app = express();
 const cors = require("cors");
+const path = require("path");
+
 //Connect Databae
 connectDB();
+
 // init middleware
 app.use(express.json({ extended: false }));
-
-app.get(`/`, (req, res) => res.send("API Running"));
 
 // Define Routes
 
@@ -15,8 +16,17 @@ const stockRouter = require("./routes/api/stocks");
 
 app.use("/stocks", stockRouter);
 
-const PORT = process.env.PORT || 5000;
-
 app.use(express.json());
+
+//Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  //Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server connected on port ${PORT}`));
