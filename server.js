99b54b7973221f2d8ba4993ae2eb46f40c766652
Your Name <you@example.com>
2020-06-26 23:27:09 -0500

    testing

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ app.use(express.json({ extended: false }));
 
 const stockRouter = require("./routes/api/stocks");
 app.use("/stocks", stockRouter);
+app.use(express.json());
 app.use("/comments", require("./routes/api/comments"));
-
 app.use(express.json());
 
 //Serve static assets in production
