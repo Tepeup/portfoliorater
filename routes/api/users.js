cf564875af8e02a83cda8cf71915c299629d8b9a
Your Name <you@example.com>
2020-06-29 19:11:20 -0500

    testing routes

--- routes/api/users.js
+++ routes/api/users.js
@@ -14,62 +14,20 @@ router.route("/").get((req, res) => {
     .then((stocks) => res.json(stocks))
     .catch((err) => res.status(400).json("Error: " + err));
 });
-// POST request
-// Register user
 
-router.post(
-  "/add",
-  [
-    check("name", "Name is required").not().isEmpty(),
-    check("email", "Please include a valid email").isEmail(),
-    check(
-      "password",
-      "Please enter a password with 6 or more characters"
-    ).isLength({ min: 6 }),
-  ],
-  async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
+router.route("/add").post((req, res) => {
+  const username = req.body.username;
+  const email = req.body.email;
+  const password = req.body.password;
 
-    const { name, email, password } = req.body;
+  const newUser = new User({
+    username,
+    email,
+    password,
+  });
 
-    try {
-      let user = await user.findOne({ email });
-
-      if (user) {
-        return res
-          .status(400)
-          .json({ error: [{ msg: "User already exists" }] });
-      }
-      user = new User({
-        name,
-        email,
-        password,
-      });
-      const salt = await bcrypt.genSalt(10);
-      user.password = await bcrypt.hash(password, salt);
-      await user.save();
-
-      const payload = {
-        user: {
-          id: user.id,
-        },
-      };
-      jwt.sign(
-        payload,
-        config.get("jwtSecret"),
-        { expiresIn: 360000 },
-        (err, token) => {
-          if (err) throw err;
-          res.json({ token });
-        }
-      );
-    } catch (err) {
-      res.status(500).send("Server error");
-    }
-  }
-);
-
-module.exports = router;
+  newUser
+    .save()
+    .then((e) => res.send(e))
+    .catch((err) => res.status(400).json("Error: 400 " + err));
+});
