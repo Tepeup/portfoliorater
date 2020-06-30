10fbe9ec205f848cc6a589590436f6fb2ba13136
Your Name <you@example.com>
2020-06-29 20:29:25 -0500

    trying to not put in bracket

--- routes/api/users.js
+++ routes/api/users.js
@@ -60,7 +60,7 @@ router
           { expiresIn: 360000 },
           (err, token) => {
             if (err) throw err;
-            res.json({ token });
+            res.json(token);
           }
         );
       });
