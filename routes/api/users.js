c98227982635487e7dbe13cfafc79b117f3886b8
Your Name <you@example.com>
2020-06-29 19:39:54 -0500

    trying hard

--- routes/api/users.js
+++ routes/api/users.js
@@ -10,25 +10,74 @@ let User = require("../../models/Users");
 
 //GEt user test
 router.route("/").get((req, res) => {
-  Stock.find()
-    .then((stocks) => res.json(stocks))
+  User.find()
+    .then((users) => res.json(users))
     .catch((err) => res.status(400).json("Error: " + err));
 });
 
-router.route("/add").post((req, res) => {
-  const username = req.body.username;
-  const email = req.body.email;
-  const password = req.body.password;
+router
+  .route("/add", [
+    check("name", "Name is required").not().isEmpty(),
+    check("email", "Please include a valid email").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 6 or more characters"
+    ).isLength({ min: 6 }),
+  ])
+  .post(async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-  const newUser = new User({
-    username,
-    email,
-    password,
+    const username = req.body.username;
+    const email = req.body.email;
+    const password = req.body.password;
+
+    try {
+      let user = await user.findOne({ email });
+
+      if (user) {
+        return res
+          .status(400)
+          .json({ error: [{ msg: "User already exists" }] });
+      }
+      user = new User({
+        name,
+        email,
+        password,
+      });
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+      await user.save();
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      res.status(500).send("Server error");
+    }
+
+    // const newUser = new User({
+    //   username,
+    //   email,
+    //   password,
+    // });
+
+    // newUser
+    //   .save()
+    //   .then((e) => res.send(e))
+    //   .catch((err) => res.status(400).json("Error: 400 " + err));
   });
-
-  newUser
-    .save()
-    .then((e) => res.send(e))
-    .catch((err) => res.status(400).json("Error: 400 " + err));
-});
 module.exports = router;
